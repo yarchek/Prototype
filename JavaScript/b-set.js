966eefc7bc447440f1e0ad5d1707fe1957688f5d
yarchek <31922962+yarchek@users.noreply.github.com>
2017-11-13 21:52:36 +0200

    Homework
    I rewrite the class through prototypes.

--- JavaScript/b-set.js
+++ JavaScript/b-set.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/*
 class Rect {
   toString() {
     return `[${this.x}, ${this.y}, ${this.width}, ${this.height}]`;
@@ -11,3 +11,17 @@ Object.setPrototypeOf(p1, Rect.prototype);
 //p1.__proto__ = Rect.prototype;
 
 console.log(p1.toString());
+*/
+function Rect(x, y, width, height) {
+  this.x = x;
+  this.y = y;
+  this.width = width;
+  this.height = height;
+}
+
+Rect.prototype.toString = function() {
+    return `[${this.x}, ${this.y}, ${this.width}, ${this.height}]`;
+};
+
+const r = new Rect(1, 2, 20, 20);
+console.log(r);
